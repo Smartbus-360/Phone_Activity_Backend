566c29586bece0f1d0ee4dce562a8caea115bb93
Smartbus-360 <smartbus365@gmail.com>
2025-09-23 13:22:43 +0530

    Update PhoneActivity.js

--- models/PhoneActivity.js
+++ models/PhoneActivity.js
@@ -13,9 +13,21 @@ const PhoneActivity = sequelize.define(
       type: DataTypes.STRING(100),
       allowNull: false,
     },
-    activity: {
-      type: DataTypes.TEXT,
-      allowNull: true, // आपकी table में nullable है
+    battery: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    screen_state: {
+      type: DataTypes.STRING(10), // "ON" or "OFF"
+      allowNull: true,
+    },
+    foreground_app: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+    },
+    data_usage_mb: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
     },
     created_at: {
       type: DataTypes.DATE,
