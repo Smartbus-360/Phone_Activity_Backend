52d2967c4e9e37c434156391e96ca24ee647f4e9
Smartbus-360 <smartbus365@gmail.com>
2025-09-23 14:19:31 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,22 @@
+// models/User.js
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+import bcrypt from "bcrypt";
+
+const User = sequelize.define("User", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  username: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.ENUM("superadmin", "schooladmin"), defaultValue: "schooladmin" },
+  school_id: { type: DataTypes.INTEGER, allowNull: false }
+}, {
+  tableName: "users",
+  timestamps: true
+});
+
+// 🔹 Hash password before saving
+User.beforeCreate(async (user) => {
+  user.password = await bcrypt.hash(user.password, 10);
+});
+
+export default User;
