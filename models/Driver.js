14a94a5fdbab209f3dc460fa76a37fd952733a08
Smartbus-360 <smartbus365@gmail.com>
2025-09-30 10:33:50 +0530

    Update Driver.js

--- models/Driver.js
+++ models/Driver.js
@@ -2,13 +2,18 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 import School from "./School.js";
+import PhoneActivity from "./PhoneActivity.js";
+import bcrypt from "bcrypt";   
+
+
 
 const Driver = sequelize.define("Driver", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   name: { type: DataTypes.STRING, allowNull: false },
     username: { type: DataTypes.STRING, allowNull: false, unique: true },  // NEW
   password: { type: DataTypes.STRING, allowNull: false },
-  device_id: { type: DataTypes.STRING, allowNull: false, unique: true }
+  device_id: { type: DataTypes.STRING, allowNull: false, unique: true },
+  school_id: { type: DataTypes.INTEGER, allowNull: true }
 }, {
   tableName: "drivers",
   timestamps: true
@@ -21,5 +26,7 @@ Driver.beforeCreate(async (driver) => {
 // 🔹 A driver belongs to a school
 Driver.belongsTo(School, { foreignKey: "school_id" });
 School.hasMany(Driver, { foreignKey: "school_id" });
+Driver.hasMany(PhoneActivity, { foreignKey: "DriverId" });
+PhoneActivity.belongsTo(Driver, { foreignKey: "DriverId" });
 
 export default Driver;
